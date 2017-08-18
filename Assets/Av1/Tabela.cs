772ee73c651e2beea8e829dff649bddf70d70bd3
Thiago Barcellos Mattos <thiago.mattos@cejoseleitelopes.com.br>
2017-08-18 10:14:00 -0300

    Av1
    Tabela de duas colunas no editor

---
+++ Assets/Av1/Tabela.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tabela : MonoBehaviour {
+	public List<int> tabela = new List<int>();
+	public List<string> tabelastring = new List<string>();
+
+	public class entry
+	{
+		public string nome;
+		public int valor;
+
+		public entry (string n, int v)
+		{
+			nome = n;
+			valor = v;
+		}
+	}
+
+	public List<entry> tabela1 = new List<entry>();
+
+	public void Reset()
+	{
+		tabela1.Clear ();
+		tabela1.Add (new entry("", 0));
+
+		/*    com duas listas
+		tabelastring.Clear ();
+		tabela.Clear();
+		tabelastring.Add ("ghjhrgsefad");		
+		tabela.Add (0);*/
+	}
+
+	public void addline(){
+		/*   com duas listas
+		tabelastring.Add ("");
+		tabela.Add (0);*/
+		tabela1.Add (new entry ("",0));
+	}
+
+	public void removeline(){
+		/*   com duas listas
+		tabelastring.Remove ("");
+		tabela.Remove (0);*/
+		tabela1.RemoveAt(tabela1.Count - 1);
+	}
+
+}
---
+++ Assets/Editor/TabelaEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Tabela))]
+
+public class TabelaEditor : Editor {
+	public override void OnInspectorGUI(){
+		Tabela tb = target as Tabela;
+
+		for (int i = 0; i < tb.tabela1.Count; i++) {
+			EditorGUILayout.BeginHorizontal ();
+			tb.tabela1[i].nome = EditorGUILayout.TextField (tb.tabela1[i].nome);
+			tb.tabela1[i].valor = EditorGUILayout.IntField (tb.tabela1[i].valor);
+			EditorGUILayout.EndHorizontal ();
+
+			/*Com duas lists
+
+			EditorGUILayout.BeginHorizontal ();
+			tb.tabelastring[i] = EditorGUILayout.TextField (tb.tabelastring[i]);
+			tb.tabela[i] = EditorGUILayout.IntField (tb.tabela[i]);
+			EditorGUILayout.EndHorizontal ();
+			*/
+
+		}
+
+		if (GUILayout.Button ("Adicionar linha")) {
+			tb.addline();
+		}
+		if (GUILayout.Button ("Remover linha")) {
+			tb.removeline();
+		}
+	}
+}
